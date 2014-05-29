75c378711babe48653b9d859b1f13d9586cefe38
Zack Fisch <zfiscr@gmail.com>
2014-05-28 23:31:42 -0400

    Delete spacing.rb

--- spacing.rb
+++
@@ -1,7 +0,0 @@
-linewidth = 40
-linewidth2 = 15
-linewidth3 = 20
-puts "Table of Conents".center(linewidth)
-puts "Chapter 1: " + "Numbers ".rjust(linewidth2) + "page 1".rjust(linewidth3)
-puts "Chapter 2: " + "Letters ".rjust(linewidth2) + "page 2".rjust(linewidth3)
-puts "Chapter 3: " + "Variables ".rjust(linewidth2) + "page 118".rjust(linewidth3)
